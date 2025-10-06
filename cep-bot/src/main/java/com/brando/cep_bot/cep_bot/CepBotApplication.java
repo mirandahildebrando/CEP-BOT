2b43624e2d41bcdb19073b473adbe98841c8975f
mirandahildebrando <mirandahildebrando5@gmail.com>
2025-10-06 17:35:16 -0300

    first commit

---
+++ cep-bot/src/main/java/com/brando/cep_bot/cep_bot/CepBotApplication.java
@@ -0,0 +1,13 @@
+package com.brando.cep_bot.cep_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CepBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CepBotApplication.class, args);
+	}
+
+}
---
+++ cep-bot/src/test/java/com/brando/cep_bot/cep_bot/CepBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brando.cep_bot.cep_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
